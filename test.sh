20a839374053bcc8a014f49f76ac2eb5ceb30b01
maheedhar132 <maheedhartalluri@gmail.com>
2020-01-12 23:28:20 +0530

    1

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 nf=4
+na=2
 f=( default 4 2 1 3 )
 a=( text.txt text1.txt a.txt)
 for i in "${a[@]}"
  do 
  for j in "${f[@]}"
  do
-	 FILE=/home/ec2-user/${f[$j]}/a{a[$i]}
- 	 if test -f "$FILE"; then
+	 FILE=/home/ec2-user/${f[$j]}/{a[$i]}
+ 	echo "$FILE" 
+                if test -f "$FILE"; then
 	 echo "$FILE exist"
 	 break
  	fi
