bc8a8f12a58546ebb05fc4eadca495572c9e536f
maheedhar132 <maheedhartalluri@gmail.com>
2020-01-12 23:13:31 +0530

    1

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
-
+nf=4
 a=( default 4 2 1 3 )
-
-for i in {0..4..1}
+f=( text.txt text1.txt a.txt)
+for i in {0..$nf..1}
  do 
-FILE=/home/ec2-user/${a[$i]}/text.txt
-if test -f "$FILE"; then
-    echo "$FILE exist"
-fi
+ for j in {0..3..1}
+ do
+	 FILE=/home/ec2-user/${a[$i]}/${f[$j]}
+ 	 if test -f "$FILE"; then
+	 echo "$FILE exist"
+	 break
+ 	fi
+ done
 done
