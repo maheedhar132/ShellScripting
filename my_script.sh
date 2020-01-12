e858a5b23b415ccc535cabf7fa35f7008dadcfd1
maheedhar132 <maheedhartalluri@gmail.com>
2020-01-13 00:26:05 +0530

    1

---
+++ my_script.sh
@@ -0,0 +1,27 @@
+#getting the total number of files
+read -p "Enter total number of files: " nf
+
+#Getting the file names
+read -a a
+
+
+
+#folder array
+f=( default 4 2 1 3 )
+
+#create a text file for storing path
+sudo cat  > result.txt
+
+#check files name wise
+for i in "${a[@]}"
+ do 
+ for j in "${f[@]}"
+   do
+   
+    FILE=/home/ec2-user/${f[$j]}/$i
+    if test -f "$FILE"; then
+       echo "$i	::	$FILE" >> result.txt
+       break
+    fi
+   done
+done	 
