1744e8c6243861a336b3700da708c5a81747f097
MaheedharTalluri <1210815148@gitam.in>
2020-01-13 15:23:56 +0530

    tomcat bootstrapped

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -6,30 +6,30 @@ sudo yum install java
 #downloading tomcat
 wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.50/bin/apache-tomcat-8.5.50.tar.gz
 #unzipping tar file 
-tar -xvzf apache-tomcat-8.5.49.tar.gz
+tar -xvzf apache-tomcat-8.5.50.tar.gz
 #remove tomcat downloaded tar file
-rm -rf apache-tomcat-8.5.49.tar.gz
+rm -rf apache-tomcat-8.5.50.tar.gz
 #changing the port for tomcat
-sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.49/conf/server.xml
+sed -i 's/port="8080"/port="8880"/' apache-tomcat-8.5.50/conf/server.xml
 
 #creating users in tomcat-users.xml
 
-sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.49/conf/tomcat-users.xml
+sed -i 's\</tomcat-users>\<!-- -->\g' apache-tomcat-8.5.50/conf/tomcat-users.xml
 
-echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
-echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
-echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
-echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
-echo '</tomcat-users>' >> apache-tomcat-8.5.49/conf/tomcat-users.xml
+echo '<role rolename="manager-gui" />' >> apache-tomcat-8.5.50/conf/tomcat-users.xml
+echo '<user username="admin" password="admin" roles="manager-gui" />' >> apache-tomcat-8.5.50/conf/tomcat-users.xml
+echo '<role rolename="manager-script" />' >> apache-tomcat-8.5.50/conf/tomcat-users.xml
+echo '<user username="script" password="script" roles="manager-script" />' >> apache-tomcat-8.5.50/conf/tomcat-users.xml
+echo '</tomcat-users>' >> apache-tomcat-8.5.50/conf/tomcat-users.xml
 
 #setting valve in comments
 
-sed -i 's/<Valve /<!-- <Valve /' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+sed -i 's/<Valve /<!-- <Valve /' apache-tomcat-8.5.50/webapps/manager/META-INF/context.xml
 
-sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.50/webapps/manager/META-INF/context.xml
 
 #running tomcat
-apache-tomcat-8.5.49/bin/./startup.sh
+apache-tomcat-8.5.50/bin/./startup.sh
 
 #Bootstrapping tomcat 
 sudo touch /etc/init.d/tomcat8.sh
