f2d61cbe15a893e2a8f6d832e1335222e06fec04
MaheedharTalluri <1210815148@gitam.in>
2020-01-13 15:12:39 +0530

    tomcat bootstrapped

--- tomcatinstall.sh
+++ tomcatinstall.sh
@@ -30,3 +30,26 @@ sed -i 's\:1" />\:1" /> -->\g' apache-tomcat-8.5.49/webapps/manager/META-INF/con
 
 #running tomcat
 apache-tomcat-8.5.49/bin/./startup.sh
+
+#Bootstrapping tomcat 
+sudo touch /etc/init.d/tomcat8.sh
+
+echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin" >> /etc/init.d/tomcat8.sh
+
+echo "start() {" >> /etc/init.d/tomcat8.sh
+echo " sh /usr/share/tomcat7/bin/startup.sh" >> /etc/init.d/tomcat8.sh
+echo "}" >> /etc/init.d/tomcat8.sh
+
+echo "stop() {" >> /etc/init.d/tomcat8.sh
+echo " sh /usr/share/tomcat7/bin/shutdown.sh" >> /etc/init.d/tomcat8.sh
+
+echo "}" >>/etc/init.d/tomcat8.sh
+
+echo "case $1 in" >> /etc/init.d/tomcat8.sh
+echo "  start|stop) $1;;" >> /etc/init.d/tomcat8.sh
+echo "  restart) stop; start;;" >> /etc/init.d/tomcat8.sh
+echo "  *) echo "Run as $0 <start|stop|restart>"; exit 1;;" >> /etc/init.d/tomcat8.sh
+echo "esac" >> /etc/init.d/tomcat8.sh
+
+
+sudo chmod +x /etc/init.d/tomcat8.sh
